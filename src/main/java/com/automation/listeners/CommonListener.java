07ec8435956850292631699f40643a2d70083ea8
Tanky Khanal <tanki60@gmail.com>
2017-06-23 11:13:05 -0400

    fixing the listener class and modify pom

--- src/main/java/com/automation/listeners/CommonListener.java
+++ src/main/java/com/automation/listeners/CommonListener.java
@@ -1,90 +1,99 @@
 package com.automation.listeners;
 
 import org.testng.IInvokedMethod;
-
 import org.testng.IInvokedMethodListener;
-
 import org.testng.ISuite;
-
 import org.testng.ISuiteListener;
-
 import org.testng.ITestContext;
-
 import org.testng.ITestListener;
-
 import org.testng.ITestNGMethod;
-
 import org.testng.ITestResult;
-
 import org.testng.Reporter;
 
 public class CommonListener implements ITestListener, ISuiteListener, IInvokedMethodListener {
 
-	// This belongs to ISuiteListener and will execute before the Suite start
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		Reporter.log("Completed executing test " + result.getName(), true);
 
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		Reporter.log("Completed executing test " + result.getName(), true);
+
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		Reporter.log("Completed executing test " + result.getName(), true);
+
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		printTestResults(result);
+
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// Reporter.log("Completed executing test " + result.getName(), true);
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		Reporter.log("Completed executing test " + context.getName(), true);
+
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		Reporter.log("Completed executing test " + context.getName(), true);
+
+	}
+
+	@Override
 	public void onStart(ISuite suite) {
-
-		Reporter.log("About to begin executing Suite " + suite.getName(), true);
-
-	}
-
-	// This belongs to ISuiteListener and will execute, once the Suite is
-	// finished
-
-	// This belongs to ITestListener and will execute before starting of Test
-	// set/batch
-
-	public void onStart(ITestContext arg0) {
-
-		Reporter.log("About to begin executing Test " + arg0.getName(), true);
+		// TODO Auto-generated method stub
+		Reporter.log("Completed executing test " + suite.getName(), true);
 
 	}
 
-	// This belongs to ITestListener and will execute, once the Test set/batch
-	// is finished
-
-	public void onFinish(ITestContext arg0) {
-
-		Reporter.log("Completed executing test " + arg0.getName(), true);
+	@Override
+	public void onFinish(ISuite suite) {
+		// TODO Auto-generated method stub
+		Reporter.log("Completed executing test " + suite.getName(), true);
 
 	}
 
-	// This belongs to ITestListener and will execute only when the test is pass
+	@Override
+	public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
+		// TODO Auto-generated method stub
+		String textMsg = "About to begin executing following method : " + returnMethodName(method.getTestMethod());
 
-	public void onTestSuccess(ITestResult arg0) {
-
-		// This is calling the printTestResults method
-
-		printTestResults(arg0);
-
-	}
-
-	// This belongs to ITestListener and will execute only on the event of fail
-	// test
-
-	public void onTestFailure(ITestResult arg0) {
-
-		// This is calling the printTestResults method
-
-		printTestResults(arg0);
+		Reporter.log(textMsg, true);
 
 	}
 
-	// This belongs to ITestListener and will execute before the main test start
-	// (@Test)
+	@Override
+	public void afterInvocation(IInvokedMethod method, ITestResult testResult) {
+		// TODO Auto-generated method stub
+		String textMsg = "Completed executing following method : " + returnMethodName(method.getTestMethod());
 
-	public void onTestStart(ITestResult arg0) {
-
-		System.out.println("The execution of the main test starts now");
+		Reporter.log(textMsg, true);
 
 	}
 
-	// This belongs to ITestListener and will execute only if any of the main
-	// test(@Test) get skipped
+	private String returnMethodName(ITestNGMethod method) {
 
-	public void onTestSkipped(ITestResult arg0) {
-
-		printTestResults(arg0);
+		return method.getRealClass().getSimpleName() + "." + method.getMethodName();
 
 	}
 
@@ -129,46 +138,6 @@ public class CommonListener implements ITestListener, ISuiteListener, IInvokedMe
 		}
 
 		Reporter.log("Test Status: " + status, true);
-
-	}
-
-	// This belongs to IInvokedMethodListener and will execute before every
-	// method including @Before @After @Test
-
-	public void beforeInvocation(IInvokedMethod arg0, ITestResult arg1) {
-
-		String textMsg = "About to begin executing following method : " + returnMethodName(arg0.getTestMethod());
-
-		Reporter.log(textMsg, true);
-
-	}
-
-	// This belongs to IInvokedMethodListener and will execute after every
-	// method including @Before @After @Test
-
-	public void afterInvocation(IInvokedMethod arg0, ITestResult arg1) {
-
-		String textMsg = "Completed executing following method : " + returnMethodName(arg0.getTestMethod());
-
-		Reporter.log(textMsg, true);
-
-	}
-
-	// This will return method names to the calling function
-
-	private String returnMethodName(ITestNGMethod method) {
-
-		return method.getRealClass().getSimpleName() + "." + method.getMethodName();
-
-	}
-
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void onFinish(ISuite suite) {
-		// TODO Auto-generated method stub
 
 	}
 
